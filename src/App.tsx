af186b069ac4029fcd7ea1e557ba8f7a784e3e7d
Justin Abuyuan <abuyuanjustin@gmail.com>
2023-07-29 02:49:53 -0400

    website initial stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 export default function App() {
   return (
     <>
-      <h1>website template</h1>
+      <div className="body-bg-container">
+        <main>
+          <div className="main-container">
+            <div className="main-text">
+              <h1>justin abuyuan.</h1>
+              <h2>i like to code stuff.</h2>
+              <div className="divider"></div>
+              <p>an ambitious and passionate computer science student looking to expand his knowledge of programming and computers whose love for coding started from a ROBLOX game he started playing when he was 10.</p>
+            </div>
+          </div>
+        </main>
+      </div>
     </>
   )
 }
